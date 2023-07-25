11c09c59d93aee0ee3de42fc69ca6c70812bc18c
Shohag Hassan <114509978+shohagha@users.noreply.github.com>
2023-07-25 12:13:47 +0600

    Successful

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit done  <code>src/App.js</code> and save to reload.
+        </p>
+        <h2> Hi React </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
